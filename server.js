df47da8f62f3a8bd514d460051368929c9f48cef
Nand <nandpatel01218@gmail.com>
2024-02-15 21:34:41 +0530

    Added home page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 
+// Assests
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
