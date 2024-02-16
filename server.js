6dde67f83d4cfdc6ed665dd93e72bac2f0c6ad68
Nand <nandpatel01218@gmail.com>
2024-02-16 21:44:38 +0530

    Added cart, login and register pages

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ const expressLayout = require('express-ejs-layouts');
 // Assests
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+});
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+});
 
 app.listen(PORT, () => {
     console.log('Server running on port 3000');
